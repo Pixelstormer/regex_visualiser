46ac2dd1a8e6c6873fc1dd99ed0b89da47aa324e
Pixelstorm <thepixelstormer@gmail.com>
2022-07-29 19:01:12 +0100

    Update template

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'regex-visualiser-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './regex_visualiser.js',
+  './regex_visualiser_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
