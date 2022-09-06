7c06fe50b1604058832d384e79df50bf2c5ebe26
Pixelstorm <thepixelstormer@gmail.com>
2022-09-06 21:06:44 +0100

    Run clippy on wasm in CI

--- check.sh
+++ check.sh
@@ -3,9 +3,11 @@
 set -eux
 
 cargo check --workspace --all-features --all-targets
-cargo check --workspace --all-features --all-targets --target wasm32-unknown-unknown
+cargo clippy --workspace --all-features --all-targets -- -D warnings -W clippy::all
 cargo test --workspace --all-features --all-targets
 cargo test --workspace --all-features --doc
 cargo fmt --all -- --check
-cargo clippy --workspace --all-features --all-targets -- -D warnings -W clippy::all
+
+cargo check --workspace --all-features --all-targets --target wasm32-unknown-unknown
+cargo clippy --workspace --all-features --all-targets --target wasm32-unknown-unknown -- -D warnings -W clippy::all
 trunk build
